77a39eac489c921f7ac96e4aabb9f6104b0e1fc3
dejiyama <deziyama0404@gmail.com>
2020-09-29 15:06:25 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
 
-const App = () => {
-   //配列を返し、二つの配列を返す。
-   //名前の付け方は、一つ目がxであれば、二つ目はそれを設定できる意味合いを込めてsetXとする。
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const reset = () => setCount(0)
-  const double = () => setCount( count * 2 )
-  //if文を使うと長くなるので、単純な演算処理は三項演算子でできないか考える
-  const divideThree = () => setCount( count%3 === 0 ? count/3 : count )
+const App = props => {
+
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
   return (
-    //React.Fragmentは消しても問題ない。
     <>
-      <div>count: {count}</div>
-      {/* <button onClick={increment}>+1</button> */}
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment}>+1</button>
-      <button onClick={reset}>reset</button>
-      <button onClick={double}>x2</button>
-      <button onClick={divideThree}>divide by 3</button>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setPrice(props.price)}>reset</button>
+      {/* eはeventを拾っている。 */}
+      <input value={name} onChange={ e => setName(e.target.value)} />
+      <button onClick={() => setName(props.name)}>name reset</button>
     </>
   );
 }
+
+App.defaultProps = {
+  name: '',
+  price: 1000
+}
+
+
+
 
 export default App;
